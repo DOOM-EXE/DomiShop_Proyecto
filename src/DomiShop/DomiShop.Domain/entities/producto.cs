2a4508997c679f6edcbcf4bdc884f809759aadb1
doom <62922003+DOOM-EXE@users.noreply.github.com>
2024-10-26 17:42:52 -0400

    grrrrrrrrrrr

---
+++ src/DomiShop/DomiShop.Domain/entities/producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DomiShop.Domain.Models
+{
+    public class producto
+    {
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set;}
+        public string description { get; set;}
+        public string cantidad { get; set;}
+        public int precio { get; set;}
+
+    }
+}
